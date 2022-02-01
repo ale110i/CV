0db9a6068d91409a883279fa91472f0ad65e7016
ale110i <ishhenko.as@phystech.edu>
2022-02-01 22:38:34 +0300

    initial commit

---
+++ gen_pdf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install --yes --no-install-recommends \
+    texlive-fonts-recommended \
+    texlive-latex-extra \
+    texlive-fonts-extra \
+    dvipng \
+    texlive-latex-recommended \
+    texlive-base \
+    texlive-pictures \
+    texlive-lang-cyrillic \
+    texlive-science \
+    cm-super \
+    texlive-bibtex-extra
+
+# texlive-generic-recommended \
+# texlive-generic-extra \
+
+cd src
+pdflatex -interaction=nonstopmode main.tex
